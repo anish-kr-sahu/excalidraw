47fdebd2e87a1c84e67af74c0b0bbc4f75d0e2b7
anish <anishkr0098@gmail.com>
2025-11-13 09:32:29 +0530

    add db

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,7 +1,55 @@
-import express from "express";
 
+import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config";
+import { userMiddleware } from "./middleware";
+import { CreateUserSchema, SigninSchema, CreateRoomSchema } from "@repo/common/types"
+import { prismaClient } from "@repo/db/client"
 
 const app = express();
 
+app.post ("/signup", (req,res) =>{
+    const data = CreateUserSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            msg: "Incorrect Inputs"
+        })
+        return;
+    }
+    res.json({
+        userId: "123"
+    })
+})
+
+app.post("signin", (req, res) =>{
+    const data = SigninSchema.safeParse(req.body);
+    if(!data.success) {
+        res.json({
+            msg: "Incorrect Inputs"
+        })
+        return;
+    }
+    const userId = 1;
+    const token = jwt.sign({
+        userId
+    },JWT_SECRET);
+    res.json({
+        token
+    })
+})
+
+app.post("/room",userMiddleware, (req,res) => {
+    const data = CreateRoomSchema.safeParse(req.body);
+    if(!data.success) {
+        res.json({
+            msg: "Incorrect inputs"
+        })
+        return;
+    }
+    res.json({
+        roomId: 123
+    })
+
+})
 
 app.listen(3001);
--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -1,6 +1,6 @@
 import type { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
 
 export const userMiddleware = (req: Request, res: Response, next: NextFunction) =>{
     const authHeader = req.headers.authorization;
